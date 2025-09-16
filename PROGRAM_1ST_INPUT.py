366717f240742728b6d359ee86cf8768acbd67c2
Ajin Saji <ajinsaji2004@gmail.com>
2025-09-16 15:21:58 +0530

    Create PROGRAM_1ST_INPUT.py

---
+++ PROGRAM_1ST_INPUT.py
@@ -0,0 +1,9 @@
+'''
+Autor : Ajin
+Date : 18/10/2024
+Description : program that asks the user to input their name and age. After receiving the input,display a message that includes the user's name and age
+Hello, John! YOU ARE 25 YEARS OLD
+'''
+x=input("enter your name:")
+y=int(input("enter your age:"))
+print("Hello,",x+"! you are" ,y,"years old")
