5547b8a91ff386bd337a2cf38e5c711339731ba1
Ajin Saji <ajinsaji2004@gmail.com>
2025-09-16 15:22:33 +0530

    Create ajin saji.py

---
+++ ajin saji.py
@@ -0,0 +1,43 @@
+#print("hello, world!")
+print("hello")
+print("world!")
+print("hello\nWorld")
+if 5 > 2:
+ print("five is greater than two!")
+if 5 > 2:
+    print("five is greater than two!")
+x = 5
+x = "thomas"
+print(x)
+
+x = str(3) # x will be '3'
+y = int(3) #y will be 3
+z = float(3) # z will be 3.0
+print(x)
+print(y)
+print(z)
+x = 1j
+y = "john"
+print(type(x))
+print(type(y))
+a = 4
+A = "THOMAS"
+print(A)
+
+
+
+x, y, z = "orange" , "banana" , "cherry"
+print(x)
+print(y)
+x = "python "
+y = "is "
+z = "awesome"
+print (x + y+ z)
+username = input("Enter username:")
+print("username is: " + usernamee)
+a = "Hello , world!"
+print(a[6])
+b= "hello, world!"
+print(b[2:6])
+
+
