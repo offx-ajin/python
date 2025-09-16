faf21d120d86b32632f5439b92af61235f1ed841
Ajin Saji <ajinsaji2004@gmail.com>
2025-09-16 15:50:42 +0530

    Create substring.py

---
+++ substring.py
@@ -0,0 +1,10 @@
+'''
+Author:Ajin Saji
+Date: 18/10/2024
+Decription: Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string.
+Finally, display the new string.
+'''
+string1 ="Hello, world! "
+string2=  " Everyone!"
+z=string1[7:11]
+print(z+string2)
