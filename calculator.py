24b505d920a8cc322868f93dbceb69ce4f85438f
Ajin Saji <ajinsaji2004@gmail.com>
2025-09-16 15:23:48 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,13 @@
+'''
+Author : Ajin Saji
+Date :19/10/2024
+Description:simple desktop calculator using Python.'''
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+num3=int(input("enter the third numberL:"))
+print("The sum of num1 and num2 is :",num1+num2)
+print("The difference when num2 is subtracted from num1 is:",num1-num2)
+print("product of num1 and num2 is:",num1*num2)
+print("the quotient when num1 is divided by num2",num1/num2)
+print("The remainder when num1 is divided by num2:",num1%num2)
+print("The result of (num1 + num2) * num3 / 2 is: ",(num1 + num2) * num3/2)
